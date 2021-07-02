11f42cfee1b675555ff20f6efd3f2b821ef50f1c
Phu Ngo <ptn123@gmail.com>
2021-07-02 13:58:28 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React,{useState, useEffect,useRef} from 'react';
-import 'antd/dist/antd.css';
-import './App.css';
-import { Layout, Menu, Breadcrumb ,Row ,Col,Switch as Switchs} from 'antd';
+import React, { useState, useEffect, useRef } from "react";
+import "antd/dist/antd.css";
+import "./App.css";
+import { Layout, Menu, Breadcrumb, Row, Col, Switch as Switchs } from "antd";
 import {
   DesktopOutlined,
   PieChartOutlined,
@@ -16,53 +16,44 @@ import {
   SettingOutlined,
   LogoutOutlined,
 } from "@ant-design/icons";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import HeaderMain from "./components/header"
-import Dashboard from "./components/dashboard"
-import Loader from "./components/Loader"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HeaderMain from "./components/header";
+import Dashboard from "./components/dashboard";
+import Loader from "./components/Loader";
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
-const App =(props)=>{
- const [loading,setloading] = useState(true)
-   const [collapsed,setCollapsed] =  useState(false)
-   const [theme,setTheme] =  useState("light")
-   const [width, setWidth] = useState(0);
+const App = (props) => {
+  const [loading, setloading] = useState(true);
+  const [collapsed, setCollapsed] = useState(false);
+  const [theme, setTheme] = useState("light");
+  const [width, setWidth] = useState(0);
   const ref = useRef();
 
   useEffect(() => {
-      window.setTimeout(()=>{
-        setloading(false)
-
-      },3500)
+    window.setTimeout(() => {
+      setloading(false);
+    }, 3500);
     let style = getComputedStyle(document.querySelector("body"));
     setWidth(parseInt(style.width));
     window.addEventListener("resize", () => {
-        let style = getComputedStyle(document.querySelector("body"));
-        setWidth(parseInt(style.width));
+      let style = getComputedStyle(document.querySelector("body"));
+      setWidth(parseInt(style.width));
     });
-
   }, []);
 
-  const onCollapse = collapsed => {
-    setCollapsed(collapsed)
+  const onCollapse = (collapsed) => {
+    setCollapsed(collapsed);
   };
 
-if(loading){
-  return <Loader></Loader>
-}
-    return (
-      <Router>
-
-      <Layout style={{ minHeight: '100vh' }} ref={ref}>
-
-            <Sider
-            breakpoint="md"
+  if (loading) {
+    return <Loader></Loader>;
+  }
+  return (
+    <Router>
+      <Layout style={{ minHeight: "100vh" }} ref={ref}>
+        <Sider
+          breakpoint="md"
           theme={theme}
           collapsible
           collapsed={collapsed}
@@ -71,11 +62,25 @@ if(loading){
           collapsedWidth={0}
           width={width > 600 ? "300px" : "200px"}
         >
-          <div className={!collapsed?"logo":"logo-collapsed"} style={{color:`${theme=="dark"?"white":"black"}`,paddingLeft:"10px"}}>
+          <div
+            className={!collapsed ? "logo" : "logo-collapsed"}
+            style={{
+              color: `${theme == "dark" ? "white" : "black"}`,
+              paddingLeft: "10px",
+            }}
+          >
             Artemis
+          </div>
+          <Row className="headliners">
+            <div
+              style={{
+                fontSize: `${collapsed ? ".6rem" : "1rem"}`,
+                color: `${theme == "dark" ? "white" : "black"}`,
+              }}
+            >
+              {" "}
+              Main{" "}
             </div>
-          <Row  className="headliners">
-          <div style={{fontSize:`${collapsed? ".6rem" : "1rem"}`,color:`${theme=="dark"?"white":"black"}`}}> Main </div>
           </Row>
           <Menu theme={theme} defaultSelectedKeys={["1"]} mode="inline">
             <Menu.Item key="1" icon={<PieChartOutlined />}>
@@ -105,20 +110,40 @@ if(loading){
               <Menu.Item key="10">Page1</Menu.Item>
               <Menu.Item key="11">Page2</Menu.Item>
             </SubMenu>
-      <Row className="headliners">
-        <div style={{fontSize:`${collapsed? ".6rem" : "1rem"}`,color:`${theme=="dark"?"white":"black"}`}}> Secondary </div>
-        </Row>
+            <Row className="headliners">
+              <div
+                style={{
+                  fontSize: `${collapsed ? ".6rem" : "1rem"}`,
+                  color: `${theme == "dark" ? "white" : "black"}`,
+                }}
+              >
+                {" "}
+                Secondary{" "}
+              </div>
+            </Row>
 
-            <Menu.Item key="12" icon={<QuestionCircleOutlined  />}>
+            <Menu.Item key="12" icon={<QuestionCircleOutlined />}>
               Support Center
             </Menu.Item>
-            <Menu.Item key="13" icon={<InboxOutlined  />} className="vanish-on-mobile">
+            <Menu.Item
+              key="13"
+              icon={<InboxOutlined />}
+              className="vanish-on-mobile"
+            >
               Inbox
             </Menu.Item>
-            <Menu.Item key="14" icon={<FolderOpenOutlined />} className="vanish-on-mobile" >
+            <Menu.Item
+              key="14"
+              icon={<FolderOpenOutlined />}
+              className="vanish-on-mobile"
+            >
               File manager
             </Menu.Item>
-            <Menu.Item key="15" icon={<BarsOutlined />} className="vanish-on-mobile">
+            <Menu.Item
+              key="15"
+              icon={<BarsOutlined />}
+              className="vanish-on-mobile"
+            >
               Data List
             </Menu.Item>
             <Menu.Item key="16" icon={<SettingOutlined />}>
@@ -129,42 +154,46 @@ if(loading){
             </Menu.Item>
           </Menu>
           <Row className="toggle">
-          <Switchs checkedChildren="dhark" unCheckedChildren="light" onChange={(cheked)=>{
-            if(cheked){
-              setTheme("dark")
-            }else{
-              setTheme("light")
-            }
-            }}/>
-        </Row>
+            <Switchs
+              checkedChildren="dhark"
+              unCheckedChildren="light"
+              onChange={(cheked) => {
+                if (cheked) {
+                  setTheme("dark");
+                } else {
+                  setTheme("light");
+                }
+              }}
+            />
+          </Row>
         </Sider>
         <Layout className="site-layout" theme={theme}>
-          <Header className="site-layout-background header-wrapper" style={{ padding: 0 }} >
+          <Header
+            className="site-layout-background header-wrapper"
+            style={{ padding: 0 }}
+          >
             <HeaderMain></HeaderMain>
-            </Header>
-          <Content style={{ margin: '0 16px' }} theme={theme}>
-
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360,backgroundColor:"#f0f2f5" }}>
-
-
-                  <Switch>
-                    <Route  path="/">
-                      <Dashboard>
-
-                      </Dashboard>
-                    </Route>
-
-                  </Switch>
-
+          </Header>
+          <Content style={{ margin: "0 16px" }} theme={theme}>
+            <div
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                minHeight: 360,
+                backgroundColor: "#f0f2f5",
+              }}
+            >
+              <Switch>
+                <Route path="/">
+                  <Dashboard></Dashboard>
+                </Route>
+              </Switch>
             </div>
           </Content>
-
         </Layout>
-
       </Layout>
-      </Router>
-
-    );
-}
+    </Router>
+  );
+};
 
 export default App;
